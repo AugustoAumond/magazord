65d748e5081dbb447d604f2d47d589a78058cc58
Augusto Sturmer Aumond <80125507+AugustoAumond@users.noreply.github.com>
2025-03-08 20:53:20 -0300

    fix: ajuste da altura do paginator

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -30,8 +30,6 @@ export default function Home(){
 
     useEffect(()=>{
         setPageNumber(1);
-
-        document.documentElement.classList.add("dark");
     }, [select])
 
     return (
@@ -49,9 +47,9 @@ export default function Home(){
                         <CustomSelect options={obj} selected={select} filter={() => console.log('asd')} setSelected={setSelect}/>
 
                         <div className="flex items-center gap-3 p-1.5 b-2 bg-gradient-to-r from-[#0056A6] to-[#0587FF] rounded-2xl">
-                            <ChevronLeft  onClick={() => setPageNumber(pageNumber <= 1 ? 1 : pageNumber - 1)} color="white" className="cursor-pointer"/>
-                            <span className="text-white cursor-pointer">{pageNumber}</span>
-                            <ChevronRight onClick={() => setPageNumber(pageNumber + 1)} color="white" className="cursor-pointer"/>
+                            <ChevronLeft  onClick={() => setPageNumber(pageNumber <= 1 ? 1 : pageNumber - 1)} size={20} color="white" className="cursor-pointer"/>
+                            <span className="text-white text-sm cursor-pointer">{pageNumber}</span>
+                            <ChevronRight onClick={() => setPageNumber(pageNumber + 1)} size={20} color="white" className="cursor-pointer"/>
                         </div>
                         
                     </div>
